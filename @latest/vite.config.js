58ad39bddf7f8758bc0f8b2b22c593bcd190889f
Shaqkori <mahoney.shaqkori@gmail.com>
2025-02-11 08:34:00 +0000

    alllowed hosts

--- @latest/vite.config.js
+++ @latest/vite.config.js
@@ -5,14 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Allow external connections
-    hmr: {
-      // Use the ALB's public hostname and protocol
-      host: "myalb-671503919.us-east-1.elb.amazonaws.com",
-      protocol: "ws", // Use 'wss' if HTTPS/SSL is enabled
-      port: 80, // Use 443 for HTTPS
-    },
-    // If behind a proxy (like ALB), set the origin explicitly
-    origin: "http://myalb-671503919.us-east-1.elb.amazonaws.com",
+    allowedHosts: true,
   },
 });
